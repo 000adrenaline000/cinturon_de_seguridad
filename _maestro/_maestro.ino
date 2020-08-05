d852dc1a8c13201f3020002dcad265b4b08ce008
000adrenaline000 <52384663+000adrenaline000@users.noreply.github.com>
2020-08-04 22:44:22 -0500

    Add files via upload

---
+++ _maestro/_maestro.ino
@@ -0,0 +1,120 @@
+#include <LiquidCrystal_I2C.h>
+#include <LedControl.h>
+
+//pin control conexion serial
+const int EnTxPin =  2; // HIGH:TX y LOW:RX
+
+//asientos por arduino
+const int n=2;
+//datos leidos
+int dato[n];
+//numero de arduinos eslavos a consultar
+const int num_ardu=1;
+// direciones de arduinos esclavos orden asiento 1, 2, 3, ..., n
+const int direccion_ard[num_ardu]={101};
+
+//matriz de leds 6x8 = 48 leds
+//pin 51 is connected to the DataIn     
+//pin 52 is connected to the CLK 
+//pin 53 is connected to LOAD 
+//We have only a single MAX72XX.
+LedControl lc=LedControl(51,52,53,1);
+
+ int acumulador_alarma;
+ int acumulador_ocupacion;
+ int alarma[48]={2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
+//max de asientos 48
+
+LiquidCrystal_I2C lcd=LiquidCrystal_I2C(0x27,16,2);
+
+void setup()                          
+   {   
+      lcd.init();
+      lcd.backlight();
+      
+      //conexion serial
+      Serial.begin(9600);
+      Serial.setTimeout(100); //establecemos un tiempo de espera de 100ms
+      pinMode(EnTxPin, OUTPUT);
+      digitalWrite(EnTxPin, HIGH); //RS485 como Transmisor
+
+   /*
+   The MAX72XX is in power-saving mode on startup,
+   we have to do a wakeup call
+   */
+  lc.shutdown(0,false);
+  /* Set the brightness to a medium values */
+  lc.setIntensity(0,4);
+  /* and clear the display */
+  lc.clearDisplay(0);
+  
+   }
+
+void loop()
+   {
+  acumulador_alarma=0;
+  acumulador_ocupacion=n*num_ardu;
+
+  for(int i=0;i<num_ardu;i++){
+  //---solicitamos una lectura del sensor----------
+  Serial.print("I"); //inicio de trama
+  Serial.print(direccion_ard[i]);//direccion del esclavo (probar en tinkercad direccion con numero entero)
+  Serial.print("F"); //fin de trama
+  Serial.flush();    //Esperamos hasta que se envíen los datos
+  //----Leemos la respuesta del Esclavo-----
+  digitalWrite(EnTxPin, LOW); //RS485 como receptor
+  if(Serial.find("i")) //esperamos el inicio de trama
+  {
+      int esclavo=Serial.parseInt();  //recibimos la direccion del esclavo
+      for(int j=0;j<n;j++){
+        dato[j]=Serial.parseInt();  //recibimos el dato
+      }
+      
+      if(Serial.read()=='f' && esclavo==direccion_ard[i]) //si fin de trama y direccion son los correctos
+      {
+       for(int j=0;j<n;j++){
+        alarma[j+n*i]=dato[j];  //se almacena en la matriz
+       }
+      }
+  }
+      digitalWrite(EnTxPin, HIGH); //RS485 como Transmisor
+  //----------fin de la respuesta----------
+  delay(100);
+  }
+ 
+    for(int i=0; i<n*num_ardu;i++){
+      int val=alarma[i];             //val==1  alarma ON
+                                     //val==0  alarma OFF
+      if (val==1)                    //val==2  asiento no ocupado
+      acumulador_alarma++;
+
+      if (val==2)
+      acumulador_ocupacion--;
+    }
+    
+    lcd.setCursor(0,0);
+    lcd.print("OCUPADOS:");
+    lcd.setCursor(10,0);
+    lcd.print(acumulador_ocupacion);
+    lcd.setCursor(0,1);
+    lcd.print("ALARMA:");
+    lcd.setCursor(10,1);
+    lcd.print(acumulador_alarma);
+
+    Matriz_leds();
+    delay(1000);
+   }
+void Matriz_leds()
+   {
+    int i=0;
+    for(int row=0;row<6;row++) { 
+    for(int col=0;col<8;col++) { 
+      if(alarma[i]==1){
+      lc.setLed(0,row,col,true);
+      }else{
+      lc.setLed(0,row,col,false);
+      }
+      i++;
+    }}
+  }
+   
---
+++ sensor_esclavo/sensor_esclavo.ino
@@ -0,0 +1,144 @@
+
+//variables para guardar los datos medidos
+int asientoReading;
+int brocheReading;
+int cinturonReading; 
+
+//variables de conversion de las magnitudes medidas
+float asientoVoltage;
+float asientoResistance;
+float cinturonVoltaje;
+float cinturonResistance;
+
+//numero de asientos sensados por arduino nano, maximo 4
+const int n=2;
+
+//PINES SENSORES e INDICADORES {1,2,....,n}
+int asiento[n] = {A0,A2};  //sensor para el asiento
+int cinturon[n]= {A1,A3}; //sensor para el cinturon
+int broche[n]  = {11,8};   //sensor de broche 
+int leds[n]    = {10,7};     //led alarma de uso incorrecto del cinturon de seguridad
+int buzzer[n]  = {9,6};    //buzzer alarma de uso incorrecto del cinturon de seguridad
+
+//variables auxiliares del programa
+int Estado[n]={0,0};
+int flag_Estado[n]={0,0};
+
+//dato a enviar
+int alarma[n];
+
+unsigned long lastTime[n];  // para almacenar el tiempo actual en que se detecta el uso incorrecto del cinturon
+int tiempo_espera=10000; //5 segundos, tiempo en que el sistema debe esperar a que el usuario haga el uso correcto del cinturon
+                                      //antes de que se active las alarmas
+
+//conexion serial
+const int EnTxPin =  2;  // HIGH:TX y LOW:RX
+const int mydireccion =101; //Direccion del esclavo
+                                      
+void setup(){ //indicamos las entradas que tendra el arduino
+  
+  //sensores
+  for(int i=0;i<n;i++){
+  pinMode(asiento[i],INPUT); 
+  pinMode(cinturon[i],INPUT);
+  pinMode(broche[i],INPUT);
+  //indicadores
+  pinMode(leds[i],OUTPUT);
+  pinMode(buzzer[i],OUTPUT);
+  }
+  
+  Serial.begin(9600);
+  Serial.setTimeout(100); //establecemos un tiempo de espera de 100ms
+  pinMode(EnTxPin, OUTPUT);
+  digitalWrite(EnTxPin, LOW); //RS485 como receptor
+}
+
+void loop(){
+
+  for(int i=0;i<n;i++){
+  
+  asientoReading = analogRead(asiento[i]);
+  asientoVoltage = float(map(asientoReading,0,1023,0,5000)); 
+  asientoResistance = (10000*(5000-asientoVoltage))/asientoVoltage;
+  if(0<asientoResistance && asientoResistance<1125){ //1125-1800
+    
+     brocheReading = digitalRead(broche[i]);
+     if(brocheReading==LOW ){ //Normalmente abierto y pull-up
+      
+      cinturonReading = analogRead(cinturon[i]);
+      cinturonVoltaje = float(map(cinturonReading,0,1023,0,5000));
+      cinturonResistance = 10000*(5000-cinturonVoltaje)/cinturonVoltaje;
+      if (cinturonResistance <= 4000){ // 200g  de fuerza en adelante
+       Estado[i]=0;//sin alarma
+      }else{
+       Estado[i]=1;//alarma activada
+      }
+   
+      }else{
+        Estado[i]=2;//alarma  en activacion
+      }  
+  }else{
+    Estado[i]=3; //desocupado
+  }
+
+  if(Estado[i]==2){
+    if(flag_Estado[i]==0){
+      lastTime[i]=millis();
+      flag_Estado[i]=1;
+      alarma[i]=0;//indica ocupacion y alarma desactivada
+    }else{ 
+      if(millis() - lastTime[i] >= tiempo_espera){ //consulta de tiempo
+        digitalWrite(leds[i],HIGH);//activar alarma visual
+        digitalWrite(buzzer[i],HIGH); //activar alarma sonora  
+        alarma[i]=1;} //alarma activada en un determinado tiempo
+    }
+  }else if(Estado[i]==0){
+    digitalWrite(leds[i],LOW);//desactivar alarma visual
+    digitalWrite(buzzer[i],LOW); //desactivar alarma sonora 
+    alarma[i]=0; //indica ocupacion y alarma desactivada
+    flag_Estado[i]=0;
+  }else if(Estado[i]==3){
+    digitalWrite(leds[i],LOW);//desactivar alarma visual
+    digitalWrite(buzzer[i],LOW); //desactivar alarma sonora 
+    alarma[i]=2;//indica asiento no ocupado
+    flag_Estado[i]=0;
+  }else if(Estado[i]==1){
+    digitalWrite(leds[i],HIGH);//activar alarma visual
+    digitalWrite(buzzer[i],HIGH); //activar alarma sonora 
+    alarma[i]=1;//alarma activada instantaneamente
+  }
+  
+  }
+
+}
+
+void serialEvent() {
+  
+    if(Serial.available())
+  {
+    if(Serial.read()=='I') //Si recibimos el inicio de trama
+    {
+        int direccion=Serial.parseInt(); //recibimos la direccion 
+        if(direccion==mydireccion) //Si direccion es la nuestra
+        {
+                if(Serial.read()=='F') //Si el fin de trama es el correcto
+                 {
+                   digitalWrite(EnTxPin, HIGH); //rs485 como transmisor
+                   Serial.print("i"); //inicio de trama  
+                   Serial.print(mydireccion); //direccion 
+                   for(int i=0;i<n;i++){
+                    Serial.print(",");       
+                    Serial.print(alarma[i]); //envio de cada elemnto del array alarma del sensor    
+                   }
+                   Serial.print("f"); //fin de trama  
+                   Serial.flush(); //Esperamos hasta que se envíen los datos
+                   digitalWrite(EnTxPin, LOW); //RS485 como receptor             
+                 }
+        }
+    }
+  }
+  delay(10); 
+}
+
+
+
